64ea99b6b340f464a606d792411c04edbef0c287
srshoruv <srshoruv2002@gmail.com>
2024-09-10 04:14:40 +0600

    implemented with Linked list

---
+++ implementationLL.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <list>
+using namespace std;
+
+template<class T>
+class Stack {
+public:
+    list<T> ll;
+    
+
+    void push(T val) {
+        ll.push_front(val);
+    }
+
+    void pop(){
+        if (!isEmpty()) {
+        ll.pop_front();
+        return;
+        } else {
+            cout << "The stack is empty! \n";
+        }
+        
+    }
+
+    T top(){       
+        return ll.front();
+
+    }
+
+
+    bool isEmpty(){
+        return ll.size() == 0;
+    }
+
+    void printStack(){
+        while (!isEmpty()) {
+            cout << ll.front() << " ";
+            ll.pop_front();
+        }
+    }
+
+};
+
+int main(){
+    Stack<string> s;
+    s.push("shoruv");
+    s.push("name is ");
+    s.push("My");
+    s.printStack();
+
+}
